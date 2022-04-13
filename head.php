8466a97144543773b197d41717e582b9ecddce7f
PepijnIjnema <Pepijn14j@gmail.com>
2022-04-13 13:59:42 +0200

    head and header.php

---
+++ head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&family=DM+Sans&family=Fredoka+One&family=Source+Code+Pro&display=swap" rel="stylesheet">
+</head>
