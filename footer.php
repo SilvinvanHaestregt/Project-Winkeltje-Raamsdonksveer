e473f1d07bdf296ab0fb6b42e118e1233d71896b
PepijnIjnema <Pepijn14j@gmail.com>
2022-04-13 14:06:04 +0200

    footer.php setup

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+    <?php require_once 'head.php'; ?>
+</head>
+<body>
+    <header>
+        <?php require_once 'header.php'; ?>
+    </header>
+    <main>
+    </main>
+    <footer>
+        <?php require_once 'footer.php'; ?>
+    </footer>
+</body>
