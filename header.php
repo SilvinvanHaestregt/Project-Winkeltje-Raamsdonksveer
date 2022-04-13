3663c521c6fc461d4b01286ac579895918977713
PepijnIjnema <Pepijn14j@gmail.com>
2022-04-13 15:06:01 +0200

    web template is af

--- header.php
+++ header.php
@@ -5,16 +5,18 @@
 <body>
 	<header>
 		<div class="header">
-			<h1>Scouting Raamsdonksveer</h1>
-			<a href="" ><img src="" alt=""></a>
-			<div class="navbar">
-				<a href="index.php">Home</a>
-				<a href="producten.php">Producten</a>
-				<a href="winkelmand.php">Winkelmandje</a>
-				<a href="contact.php">Contact</a>
-                <a href="status.php">Status</a>
-			</div>
-		</div>
+		<label>
+		<input type="checkbox">
+		<span class="menu"> <span class="hamburger"></span> </span>
+		<ul>
+  			<li> <a href="index.php">Home</a> </li>
+  			<li> <a href="producten.php">Producten</a> </li>
+  			<li> <a href="winkelmand.php">Winkelmandje</a> </li>
+  			<li> <a href="contact.php">Contact</a> </li>
+  			<li> <a href="status.php">Status</a> </li>
+		</ul>
+		</label>
+		<h1>Scouting Raamsdonksveer</h1>
 	</header>
 </body>
 </html>
