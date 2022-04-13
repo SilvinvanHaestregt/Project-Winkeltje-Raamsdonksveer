bb5eab340f229c8cc41c285b8955d9c1df25a7f2
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-04-13 14:52:07 +0200

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -10,4 +10,4 @@ $dbUser = 'root';
 $dbPass = '';
 
 //De url waarop jouw project staat. Géén slash aan het einde.
-$base_url = 'http://localhost/b3-22-feb';
+$base_url = 'http://localhost/Project-Winkeltje-Raamdsdonksveer';
