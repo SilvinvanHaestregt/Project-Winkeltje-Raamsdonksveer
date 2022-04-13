70b57fb21fcc88344add2d2eb2b63b978f392d52
PepijnIjnema <Pepijn14j@gmail.com>
2022-04-13 14:29:07 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-    <?php require_once 'head.php'; ?>
+	<link rel="stylesheet" href="css/main.css">
 </head>
-<body>
-    <header>
-        <?php require_once 'header.php'; ?>
-    </header>
-    <main>
-    </main>
-    <footer>
-        <?php require_once 'footer.php'; ?>
-    </footer>
+<body >
+	<div>
+	<p>Gemaakt door Pepijn IJnema & Silvin van Haestregt &copy;</p>
+	</div>
 </body>
+</html>
