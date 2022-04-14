7b840f3a800a4200878201ed4d703e16550f7f73
PepijnIjnema <Pepijn14j@gmail.com>
2022-04-14 11:50:50 +0200

    Update login.php

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
 </head>
 <body>
     <header>
-        <!-- <?php require_once 'header.php'; ?> -->
+        <?php require_once 'header.php'; ?>
     </header>
     <main>
         <div class="form">
