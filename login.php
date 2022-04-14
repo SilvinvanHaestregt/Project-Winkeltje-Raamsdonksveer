e272dfead35d2f41a0caf655abb18bad0952117a
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-04-14 16:03:00 +0200

    Login
    Styled login page and you can now logout and be redirected to the index.php page (mainpage)

--- login.php
+++ login.php
@@ -2,8 +2,6 @@
     session_start();
 ?>
 
-<?php require_once 'head.php'; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,34 +9,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
     <header>
-        <!-- <?php require_once 'header.php'; ?> -->
+
     </header>
     <main>
-        <div class="form">
-            <form action="backend/loginController.php" method="post">
-                <div class="form-group">
-                    <label for="username">gebruikersnaam:</label>
-                    <input type="text" name="username" id="username">
-                </div>
-                <div class="form-group">
-                    <label for="password">wachtwoord:</label>
-                    <input type="password" name="password" id="password">
-                </div>
-                <div class="form-group">
-                    <label for="name">Naam:</label>
-                    <input type="name" name="name" id="name">
-                </div>
-                <div class="form-group">
-                    <label for="email">email:</label>
-                    <input type="email" name="email" id="email">
-                </div>
-                <div class="form-group">
-                    <input type="submit" value="Submit">
-                </div>
-            </form>
+        <div class="wrapper">
+            <div class="form">
+                <form action="backend/loginController.php" method="post">
+                    <div class="form-group">
+                        <label for="username">Gebruikersnaam:</label>
+                        <input type="text" name="username" id="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Wachtwoord:</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Naam:</label>
+                        <input type="name" name="name" id="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Submit">
+                    </div>
+                </form>
+            </div>
         </div>
     </main>
     <footer>
