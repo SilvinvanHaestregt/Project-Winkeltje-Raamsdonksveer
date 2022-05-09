24aeda6b051cdf23e35ae72751f1bd114f55706f
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-05-09 11:38:14 +0200

    Update status.php

--- status.php
+++ status.php
@@ -17,6 +17,32 @@ session_start();
         <?php require_once 'header.php'; ?>
     </header>
     <main>
+        <?php
+            $order_number = 1;
+            $status = 1;
+            $producten = "Broek, schoenen, t-shirt";
+            $email_recipient = "silvinvanhaestregt@outlook.com";
+            
+            //1. Verbinden met de database
+            require_once 'backend/conn.php';
+            
+            //2. Query
+            $query = "INSERT INTO orders (order_number, status, producten, email_recipient)
+            VALUES(:order_number, :status, :producten, :email_recipient)";
+            
+            //3. Prepare
+            $statement = $conn->prepare($query);
+            
+            //4. Execute
+            $statement->execute([
+                ":order_number" => $order_number,
+                ":status" => $status,
+                ":producten" => $producten,
+                ":email_recipient" => $email_recipient
+            ]);
+            
+            $items = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
     </main>
     <footer>
         <?php require_once 'footer.php'; ?>
