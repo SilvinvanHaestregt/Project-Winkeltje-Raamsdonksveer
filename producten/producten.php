4e7632ea109e76b43f88ad6729ee3623479603f6
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-05-24 15:07:27 +0200

    Update
    Update

--- producten/producten.php
+++ producten/producten.php
@@ -9,17 +9,17 @@ session_start();
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-    <?php require_once 'head.php'; ?>
+	<link rel="stylesheet" href="../css/main.css">
+    <?php require_once '../head.php'; ?>
 </head>
 <body>
     <header>
-        <?php require_once 'header.php'; ?>
+        <?php require_once '../header.php'; ?>
     </header>
 
     <main>
         <?php
-            require_once 'backend/conn.php';
+            require_once '../backend/conn.php';
             $query = "SELECT * FROM products";
             $statement = $conn->prepare($query);
             $statement->execute();
@@ -35,9 +35,27 @@ session_start();
                 </tr>
             <?php endforeach; ?>
         </table>
+        <div class="wrapper">
+            <div class="table">
+                <table>
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Order Nummer</th>
+                        <th>Order Status</th>
+                        <th>Producten</th>
+                        <th>Besteller</th>
+                    </tr>
+                    <?php foreach ($products as $product): ?>
+                        <tr>
+                            <td><?php echo $product[''];?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+        </div>
     </main>
 
     <footer>
-        <?php require_once 'footer.php'; ?>
+        <?php require_once '../footer.php'; ?>
     </footer>
 </body>
